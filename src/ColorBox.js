713da13d87cf9b78a59585f91b5955224e9187c3
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-07-10 21:34:47 +0530

    Add Copy functionality and Animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
 import "./ColorBox.css"
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 export class ColorBox extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            copied: false
+        }
+        this.changeCopyState = this.changeCopyState.bind(this)
+    }
+
+    changeCopyState() {
+        this.setState({copied: true}, () => {
+            // inorder to stop the animation
+            setTimeout(() => {
+                this.setState({copied: false})
+            }, 1500);
+        })
+    }
+
     render() {
+        const {name, background} = this.props
+        const {copied} = this.state
         return (
-            <div style={{backgroundColor:this.props.background}} className="ColorBox">
-                <span>{this.props.name}</span>
-                <span>More</span>
-            </div>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{backgroundColor:background}} className="ColorBox">
+                    <div 
+                        style={{backgroundColor:background}} 
+                        className={`copy-overlay ${copied && 'show'}`} 
+                    />
+                    <div className={`copied-message ${copied && 'show'}`}>
+                        <h1>COPIED</h1>
+                        <p>{background}</p>
+                    </div>
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">Copy</button>
+                    </div>
+                    <span className="see-more">More</span>
+                </div>
+            </CopyToClipboard>
         )
     }
 }
