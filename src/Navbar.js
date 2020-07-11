8564465c57de36fb21a01f77a5b8e49b5610cff5
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-07-11 15:50:17 +0530

    Add Snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,20 +4,29 @@ import "./Navbar.css"
 import "rc-slider/assets/index.css"
 import Select from "@material-ui/core/Select"
 import MenuItem from "@material-ui/core/MenuItem"
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 export class Navbar extends Component {
     constructor(props){
         super(props)
         this.state = {
-            format: 'hex'
+            format: 'hex',
+            open: false
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.handleFormatChange = this.handleFormatChange.bind(this)
+        this.closeSnackbar = this.closeSnackbar.bind(this)
     }
 
-    handleChange(event){
-        this.setState({format: event.target.value})
+    handleFormatChange(event){
+        this.setState({format: event.target.value, open: true})
         this.props.handleChange(event.target.value)
+    }
+
+    closeSnackbar(){
+        this.setState({open: false})
     }
 
     render() {
@@ -41,12 +50,24 @@ export class Navbar extends Component {
                     </div>
                 </div>
                 <div className="select-container">
-                    <Select value={format} onChange={this.handleChange}>
+                    <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
                         <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>                    
                     </Select>
                 </div>
+                <Snackbar 
+                    anchorOrigin={{vertical:"bottom", horizontal:"left"}} 
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span id="message-id">Format Changed</span>}
+                    onClose={this.closeSnackbar}
+                    action={[
+                        <IconButton onClick={this.closeSnackbar} color="inherit">
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
             </header>
         )
     }
